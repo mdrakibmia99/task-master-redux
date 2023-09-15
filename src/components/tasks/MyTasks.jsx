3840b9e6150fcb706553271b5b1627877c4ed646
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-15 19:17:16 +0600

    after click detail now show detail on the modal

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,19 +2,28 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUserTask, updateStatus } from '../../redux/features/tasks/tasksSlice';
+import TaskDetailModal from './TaskDetailModal';
 
 const MyTasks = () => {
   const { name: userName } = useSelector((state) => state.userSlice)
   const { tasks,userTasks } = useSelector((state) => state.taskSlice)
+  const [isOpen,setIsOpen]=useState(false)
+  const [modalId,setModalId]=useState(0)
   const dispatch = useDispatch();
+const handleModal=(id)=>{
+  setModalId(id);
+  setIsOpen(!isOpen);
+}
+
   useEffect(() => {
     dispatch(setUserTask(userName))
   }, [userName, dispatch,tasks])
   return (
     <div>
+      <TaskDetailModal  isOpen={isOpen} setIsOpen={setIsOpen} modalId={modalId}/>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userTasks?.map(item => (
@@ -24,7 +33,7 @@ const MyTasks = () => {
           >
             <h1>{item.title}</h1>
             <div className="flex gap-3">
-              <button className="grid place-content-center" title="Details">
+              <button onClick={()=>handleModal(item.id)} className="grid place-content-center" title="Details">
                 <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
               </button>
               <button
---
+++ src/components/tasks/TaskDetailModal.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import Modal from "../ui/Modal";
+
+const TaskDetailModal = ({isOpen,setIsOpen,modalId}) => {
+    const {userTasks}=useSelector((state)=>state.taskSlice)
+    console.log(userTasks)
+    const task= userTasks.find(item=>item.id === modalId)
+    return (
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={task?.title}>
+            {task?.description}
+        </Modal>
+    );
+};
+
+export default TaskDetailModal;
