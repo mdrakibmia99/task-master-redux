cc30f046297e78ddd04224a26ff2996687b1f254
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-11-09 14:52:28 +0600

    data fetch from mongodb

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -5,14 +5,17 @@ import AddTaskModal from '../components/tasks/AddTaskModal';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MenuDropdown from '../components/ui/MenuDropdown';
+import { useGetTasksQuery } from '../redux/features/api/baseApi';
 
 const Tasks = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { tasks } = useSelector((state) => state.taskSlice);
+  // const { tasks } = useSelector((state) => state.taskSlice);
+  const {data:tasks}=useGetTasksQuery();
+  console.log(tasks,"tasksss")
   const pendingTasks = tasks?.filter((item) => item.status == 'pending');
   const runningTasks = tasks?.filter((item) => item.status == 'running');
   const doneTasks = tasks?.filter((item) => item.status == 'done');
-  const dispatch=useDispatch();
+
   
 
   return (
@@ -54,11 +57,11 @@ const Tasks = () => {
               <div className="flex sticky top-0  justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
                 <h1>Up Next</h1>
                 <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                  {pendingTasks.length}
+                  {pendingTasks?.length}
                 </p>
               </div>
               <div className="space-y-3">
-                {pendingTasks.map((item) => (
+                {pendingTasks?.map((item) => (
                   <TaskCard key={item.id} task={item} />
                 ))}
               </div>
@@ -67,11 +70,11 @@ const Tasks = () => {
               <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
                 <h1>In Progress</h1>
                 <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                  {runningTasks.length}
+                  {runningTasks?.length}
                 </p>
               </div>
               <div className="space-y-3">
-                {runningTasks.map((item) => (
+                {runningTasks?.map((item) => (
                   <TaskCard key={item.id} task={item} />
                 ))}
               </div>
@@ -80,11 +83,11 @@ const Tasks = () => {
               <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
                 <h1>Up Next</h1>
                 <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                  {doneTasks.length}
+                  {doneTasks?.length}
                 </p>
               </div>
               <div className="space-y-3">
-                {doneTasks.map((item) => (
+                {doneTasks?.map((item) => (
                   <TaskCard key={item.id} task={item} />
                 ))}
               </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksSlice from "./features/tasks/tasksSlice"
 import userSlice from "./features/users/usersSlice"
+import baseApi from "./features/api/baseApi";
 
 const store=configureStore({
     reducer:{
+        [baseApi.reducerPath]:baseApi.reducer,
         taskSlice:tasksSlice,
         userSlice:userSlice
+
     },
-})
+    middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(baseApi.middleware)})
+
 export default store;
