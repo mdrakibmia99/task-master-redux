001cbeed1529e989fff678a4eae5e8845342e6d5
mdrakibmia99 <rkrakibhasan680@gmail.com>
2023-12-06 15:35:10 +0600

    update backend live url

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
  const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://task-master-redux-backend.vercel.app' }),
     tagTypes:['Tasks'],
     endpoints: () => ({}),
 })
